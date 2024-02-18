7fd1ccc388cf3e8f85679e5f608520490a9ec85d
HakkeiZOMbie <32358417+HakkeiZOMbie@users.noreply.github.com>
2024-02-18 01:08:05 -0800

    Wrote some vague starting code for Chess.hs

---
+++ Chess.hs
@@ -0,0 +1,16 @@
+data State = State [Tile] [Flag]
+
+-- a move moves a piece from one tile to another tile
+data Move = Move Tile Tile
+
+-- just a coordinate with optionally a piece on it for now
+data Tile = Tile Int Int Piece 
+
+data Piece = Pawn | Rook | Knight | Bishop | Queen | King | Nothing
+data Flag = Castled | EnPassant
+
+-- plays a given move
+play :: Move -> State -> State
+
+-- checks whether a move is valid
+checkMove :: Move -> State -> Bool
